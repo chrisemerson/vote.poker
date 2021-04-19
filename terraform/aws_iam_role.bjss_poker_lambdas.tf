b2ee0e537860f35ddc1954f7b90de7300df6009e
Chris Emerson <chris@cemerson.co.uk>
2021-04-19 23:04:56 +0100

    Simplify AWS Roles by using same one for all Lambdas

--- terraform/aws_iam_role.bjss_poker_lambdas.tf
+++ terraform/aws_iam_role.bjss_poker_lambdas.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "bjss_poker_lambdas" {
-  name               = "Lambda-Execution-BJSSPoker-Connect"
+  name               = "Lambda-Execution-BJSSPoker"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
   path               = "/service-role/"
 }
--- terraform/data.archive_file.lambdas.tf
+++ terraform/data.archive_file.lambdas.tf
@@ -1,7 +1,7 @@
 data "archive_file" "lambdas" {
   type = "zip"
 
-  for_each = toset([for l in fileset("../lambda/", "*/index.js"): dirname(l)])
+  for_each = toset([for l in fileset("../lambda/", "*/index.js") : dirname(l)])
 
   source_dir  = "../lambda/${each.value}/"
   output_path = "../build/lambdas/${each.value}.zip"
