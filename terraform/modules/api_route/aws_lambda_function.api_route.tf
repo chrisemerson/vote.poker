69d1705848872356a4810c3d4a5ed81163d7076f
Chris Emerson <chris@cemerson.co.uk>
2021-04-27 19:04:43 +0100

    Remove API Mapping key and change lambda handler

--- terraform/modules/api_route/aws_lambda_function.api_route.tf
+++ terraform/modules/api_route/aws_lambda_function.api_route.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "api_route" {
   function_name = "bjss_poker_${var.lambda_function_name}"
 
   runtime = "nodejs14.x"
-  handler = "index.js"
+  handler = "index.handler"
 
   role = var.lambda_role.arn
 
