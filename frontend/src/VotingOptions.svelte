be1ae3346cd234beaf8c371a5f3e5a3a5f8ed6de
SiJiL82 <sven.lowry@gmail.com>
2022-10-11 22:41:31 +0100

    Made voting cards responsive

--- frontend/src/VotingOptions.svelte
+++ frontend/src/VotingOptions.svelte
@@ -3,27 +3,28 @@
 
 </script>
 
-<VotingOption value="1"/>
-<VotingOption value="2"/>
-<VotingOption value="3"/>
-<VotingOption value="5"/>
-<VotingOption value="8"/>
-<VotingOption value="13"/>
-<VotingOption value="21"/>
-<VotingOption value="34"/>
-<VotingOption value="55"/>
-<VotingOption value="89"/>
-<VotingOption value="?"/>
+<section class="voting-options">
+    <VotingOption value="1"/>
+    <VotingOption value="2"/>
+    <VotingOption value="3"/>
+    <VotingOption value="5"/>
+    <VotingOption value="8"/>
+    <VotingOption value="13"/>
+    <VotingOption value="21"/>
+    <VotingOption value="34"/>
+    <VotingOption value="55"/>
+    <VotingOption value="89"/>
+    <VotingOption value="?"/>
+</section>
 
 <style>
-    main {
+    .voting-options {
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
-        justify-content: space-between;
+        justify-content: flex-start;
         align-items: flex-end;
         width: 100%;
-        height: 400px;
         border-bottom: 1px solid black;
     }
 </style>
